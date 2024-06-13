2135530218d4eca889c4fc465c295b93c2e754f4
BURNETT <BURNETT@TVT6031.svl.ibm.com>
2024-06-13 15:40:13 -0400

    Push from z\/OS

---
+++ samples/MortgageApplication/cobol/epscz���.cbl
@@ -0,0 +1,59 @@
+   CBL NUMPROC(MIG),FLAG(I,W),RENT
+       ID DIVISION.
+       PROGRAM-ID. EPSCZÅÄÖ.
+      *    THIS IS A CALLED PROGRAM EXAMPLE FOR DEMONSTRATION
+      * 
+      *    THIS PROGRAM IS INVOKED VIA A CICS LINK STATMENT
+      *    AND DYNAMICALLY CALLS THE ACTUAL PROGRAM
+      *
+      *    (C) 2017 IBM JIM HILDNER.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SOURCE-COMPUTER. FLEX-ES.
+       OBJECT-COMPUTER. FLEX-ES.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      *
+       01  WS-CALLED-PROGRAM    PIC X(8).
+
+       01  STATIC-CALLED-PROGRAMS.
+           03 STATIC-CALLED-PROGRAM-TABLE.
+              05 FILLER            PIC X(8) VALUE 'EPSMPMT'.
+              05 FILLER            PIC X(8) VALUE 'NOT VLD'.
+              05 FILLER            PIC X(8) VALUE ' '.
+           03 CALLED-PROGRAM-TABLE
+                        REDEFINES STATIC-CALLED-PROGRAM-TABLE
+                        OCCURS 3 TIMES.
+              05 CALLED-PROGRAM-NAME PIC X(8).
+
+       COPY EPSPDÅÄÖ.
+
+       LINKAGE SECTION.
+      *
+       01 DFHCOMMAREA.
+       COPY EPSMTCOM.
+
+       PROCEDURE DIVISION USING DFHCOMMAREA.
+      *
+       A000-MAINLINE.
+           MOVE EPSPCOM-PRINCIPLE-DATA  TO EPSPDÅÄÖ-PRINCIPLE-DATA.
+           MOVE EPSPCOM-NUMBER-OF-YEARS TO EPSPDÅÄÖ-NUMBER-OF-YEARS.
+           MOVE 'Y'                     TO EPSPDÅÄÖ-YEAR-MONTH-IND.
+           MOVE EPSPCOM-QUOTED-INTEREST-RATE
+                                        TO
+                                   EPSPDÅÄÖ-QUOTED-INTEREST-RATE.
+           MOVE CALLED-PROGRAM-NAME(1)  TO WS-CALLED-PROGRAM.
+           MOVE SPACES                  TO EPSPDÅÄÖ-RETURN-ERROR.
+      *     CALL 'EPSMPMT' USING EPSPDÅÄÖ.
+           CALL WS-CALLED-PROGRAM USING EPSPDÅÄÖ.
+           MOVE EPSPDÅÄÖ-RETURN-MONTH-PAYMENT
+                                        TO
+                                        EPSPCOM-RETURN-MONTH-PAYMENT.
+           MOVE EPSPDÅÄÖ-RETURN-ERROR   TO EPSPCOM-ERRMSG.
+           IF EPSPDÅÄÖ-RETURN-ERROR = SPACES
+              MOVE ZERO TO EPSPCOM-PROGRAM-RETCODE
+           ELSE
+              MOVE 8 TO EPSPCOM-PROGRAM-RETCODE
+           END-IF.
+           GOBACK
+           .
